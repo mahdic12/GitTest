eead0bdfbc406192bda0c9d6d39dd4da49b8bfc4
mahdi <mahdi@MCH.home>
2024-01-12 11:41:47 +0100

    Test Git Commit MCH

---
+++ src/com/mch/soft/Main.java
@@ -0,0 +1,16 @@
+/**
+ * mahdi
+ * Jan 12, 2024
+ * GitTest
+ */
+package com.mch.soft;
+
+/**
+ * @author mahdi
+ *
+ */
+public class Main {
+public static void main(String[] args) {
+	System.out.println("Hello Word.......");
+}
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * mahdi
+ * Jan 12, 2024
+ * GitTest
+ */
+/**
+ * @author mahdi
+ *
+ */
+module GitTest {
+}
